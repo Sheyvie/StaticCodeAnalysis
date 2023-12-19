c65b7e98a24ab4c195864c2157bda7b9bb500da5
Sheyvie <sheilayvi@gmail.com>
2023-12-19 15:48:44 +0300

    first commit

---
+++ sample.py
@@ -0,0 +1,17 @@
+"""
+The following module provides a simple function for adding two numbers.
+"""
+
+def add(number1, number2):
+    """Add two numbers and return the result"""
+    return number1+number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+
+print(f"the sum of {NUM1} and {NUM2} is {TOTAL}")
+
+
+
+  
