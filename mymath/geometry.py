f04f3bdde6852fd5cc1fe534a5fc341c561973d6
Sheyvie <sheilayvi@gmail.com>
2023-12-20 10:11:26 +0300

    geometry module

---
+++ mymath/geometry.py
@@ -0,0 +1,14 @@
+"""Geometry operations"""
+import math 
+
+def area_of_rectangle(length,breadth):
+    """
+    This funcyions gets the area of a rectangle
+    """
+    return length*breadth
+
+def area_of_circle(radius):
+    """ 
+    This functions gets the area of a circle
+    """
+    return math.pi *radius**2
